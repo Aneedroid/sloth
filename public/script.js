eda14af3dbbf67457bccdfd05db92d8eaa225239
Aneedroid <aneedroid13@gmail.com>
2022-03-27 10:53:08 +0530

    Convert from video to screenshare

--- public/script.js
+++ public/script.js
@@ -6,9 +6,11 @@ const myVideo = document.createElement('video') // Create a new video tag to sho
 myVideo.muted = true // Mute ourselves on our end so there is no feedback loop
 
 // Access the user's video and audio
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true
+navigator.mediaDevices.getDisplayMedia({
+  video: {
+    cursor: 'always'
+  },
+  audio: false
 }).then(stream => {
   addVideoStream(myVideo, stream) // Display our video to ourselves
 
